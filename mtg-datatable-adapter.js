0b647eaad8f789b6d7534d5d23d551ffdf67af58
anishhirlekar <anish.hirlekar@ipsumworks.com>
2017-04-02 23:52:50 +0530

    Added example files

---
+++ mtg-datatable-adapter.js
@@ -0,0 +1,26 @@
+
+(function ( $ ) {
+	$.fn.getMtgDataTable = function(tableId, url){
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', url, true);
+
+		xhr.onload = function () {
+        	var dataset = [];
+        	var list = JSON.parse(xhr.responseText);
+        		
+        	$.each(list, function(index, obj) {
+        		var arr = [];
+        		$.each(obj, function(key, val) {
+	       			arr.push(val);
+        		});
+        		dataset.push(arr);
+        	});
+
+        	$(tableId).dataTable( {
+		        "aaData": dataset						
+		    });
+		}
+
+		xhr.send();	      
+	}
+}( jQuery ));
---
+++ script.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+	$(this).getMtgDataTable('#datatable', 'https://api.metamug.com/dtable/v1.0/employee');
+	
+});
